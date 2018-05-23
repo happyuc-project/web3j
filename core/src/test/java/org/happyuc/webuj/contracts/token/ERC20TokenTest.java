3f0a20951f4c07ff3e4368dfbba010210a160837
ldcc <ldcc@live.com>
2018-05-23 16:15:55 +0800

    update test

--- core/src/test/java/org/happyuc/webuj/contracts/token/ERC20TokenTest.java
+++ core/src/test/java/org/happyuc/webuj/contracts/token/ERC20TokenTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -25,35 +26,40 @@ public class ERC20TokenTest {
 
     private Credentials credentials;
 
-    public static final String NODE_URL = "http://112.74.96.198:8545";
-    public static final String _TO = "";
+    public static final String NODE_URL = "http://112.74.96.198:8550";
     public static final String PRIVATE_KEY = "";
-    public static final String SOURCE = "";
-    public static String CONTRACT_ADDR;
+    public static String CONTRACT_ADDR = "";
+
+    public static final String _TO = "0xd0ca89a6d9435a6a4857c1083f165af01fbfda7d";
+    public static final String SOURCE = "/home/ldc/blockchain/hucdev/data0/keystore" + "/UTC--2018-04-23T13-48-42.528788235Z--b3f1507591583ebf14b5b31d134d700c83c20fa1";
 
     @Before
     public void setUp() throws IOException, CipherException {
-        webuj = Webuj.build(new HttpService());
+        webuj = Webuj.build(new HttpService(NODE_URL));
         // credentials = Credentials.create(PRIVATE_KEY);
         credentials = WalletUtils.loadCredentials("123456", SOURCE);
     }
 
     @Test
-    public void deploy() throws Exception {
-        String name = "ERCJ(Test)";
-        String symbol = "ERCJ";
+    public void deploy() {
+        String name = "Token(Test)";
+        String symbol = "Token";
         BigInteger price = ManagedTransaction.GAS_PRICE;
         BigInteger limit = Contract.GAS_LIMIT;
         BigInteger amount = BigInteger.valueOf(1000000);
-        ERC20Token contract = ERC20Token.deploy(webuj, credentials, price, limit, amount, name, symbol).send();
-        assertTrue("Create new contract token", contract.isValid());
-        CONTRACT_ADDR = contract.getContractAddress();
+        ERC20Token contract;
+        try {
+            contract = ERC20Token.deploy(webuj, credentials, price, limit, amount, name, symbol).send();
+            assertTrue("Create deploy failed", contract.isValid());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void load() throws Exception {
         ERC20Token contract = ERC20Token.load(CONTRACT_ADDR, webuj, credentials);
-        assertTrue("Load existing contract token", contract.isValid());
+        assertTrue("Contract address does not exist", contract.isValid());
     }
 
     @Test
@@ -65,6 +71,13 @@ public class ERC20TokenTest {
         txsInfo.forEach(EventResponse.TransferEr::print);
         BigInteger after = contract.balanceOf(credentials.getAddress()).send();
         BigDecimal balTail = Convert.fromWei(before.subtract(after).toString(), Convert.Unit.HUC);
-        assertEquals("Transfer 1 ERC20", balTail, BigDecimal.ONE);
+        assertEquals("Transfer failed", balTail, BigDecimal.ONE);
     }
+
+    @Test
+    public void mulDeploy() {
+        List<String> names = Arrays.asList("SouthPark", "Happy Tree Friends", "");
+        names.forEach(s -> new Thread(this::deploy));
+    }
+
 }
